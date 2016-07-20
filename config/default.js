8bd5b07d1f55132384ca3376481c6752717e430e
ayabe <kayabe@salesforce.com>
2016-07-20 12:18:41 +0900

    Update default.js

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
-   appId           : '29172a9c-c893-456a-bc8c-648132e35390',
-   clientId        : 'myclientid',
-   clientSecret    : 'myclientsecret',
-   appSignature    : 'myappsignature',
+   appId           : '1742a858-ed67-46a6-9559-b678c0bbfe5f',
+   clientId        : 'l94e03x0ayp8eydvjydaunhe',
+   clientSecret    : 'Szkcuuzr0CSDBD9XlATK6eDL',
+   appSignature    : 'uozcimdsv5oobjtrhufsmphchxy3cznkxqrhxrfu5p0pal211gddkidk5hvwqc0xm1w1vybz4d3lb3qnh3h1ybeiosvge2il2mxwjsa0304mnnhp0d0z4hijad5plqv5fdzi4a1lxxvcduq0ov5a5vnh01izm0qksrxxiriqa2tpguox4sdo2wnmcx4nbhlxrsj2rhds2e21sg2altcngtidyk4tykczsrawe5o1ummariavmnwrnuski1qcd2q',
    authUrl         : 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
 };
